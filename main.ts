73140e369adc6f01a8c1115419874461a6f98bd0
ONDR10 <79906689+ONDR10@users.noreply.github.com>
2021-05-31 17:11:33 +0200

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+let mode = 0;
+
+
+
+
+
+
+basic.forever(function () {
+	if (mode) {
+        basic.showString("Vyber 1. hraca:");
+    }
+
+    if (mode) {
+        basic.showString("Vyber 2. hraca:");
+    }
+    else {
+        basic.showString("Vyber obtiaznost: ");
+    }
+
+    
+
+})
